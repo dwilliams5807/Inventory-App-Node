79a277c8f43d99ac6e96868175f7930a9b8bbdd7
Daniel Williams <daniellanierwilliams@gmail.com>
2019-06-30 19:53:34 -0500

    installed mysql, inquirer and init packages

--- bamazon.sql
+++ bamazon.sql
@@ -4,7 +4,14 @@ CREATE database bamazon;
 
 USE bamazon;
 
-CREATE TABLE products();
+CREATE TABLE products(
+    item_id INT(4) NOT NULL,
+	product_name VARCHAR(100) NOT NULL,
+	department_name VARCHAR(100) NOT NULL,
+	price DECIMAL(10,2) NOT NULL,
+	stock_quantity INT(20) NOT NULL,
+	PRIMARY KEY (item_id)
+);
 
 SELECT * FROM products
 
