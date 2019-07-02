af3ed94be10d73812cbc39c8f15a7e7aefddfbda
Daniel Williams <daniellanierwilliams@gmail.com>
2019-07-02 08:43:40 -0500

    added text to read me. Added JS functions to manipulate database

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# bamazon
+# Bamazon Store
+
+A command line inventory application with customer view.
+
+## Installation
+Be sure to privide your own .emv file with a password to your database.
+
+## ScreenShots
+
+![ScreenShot](./images/1.png)
+Format: ![Alt Text](url)
+
+![ScreenShot](./images/2.png)
+Format: ![Alt Text](url)
+
+![ScreenShot](./images/3.png)
+Format: ![Alt Text](url)
--- bamazon.sql
+++ bamazon.sql
@@ -13,5 +13,19 @@ CREATE TABLE products(
 	PRIMARY KEY (item_id)
 );
 
-SELECT * FROM products
+SELECT * FROM products;
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) 
+VALUES
+       (843, "Airpods", "Electronics", 139.99, 5),
+	   (623, "Sunglasses", "Clothing", 8.99, 10),
+	   (437, "Mens Jacket", "Clothing", 34.99, 9),
+	   (439, "Womens Jacket", "Clothing", 39.99, 14),
+	   (203, "CDB Infused Honey", "Food", 39.99, 12),
+	   (309, "Sour Patch Kids", "Candy", 2.99, 100),
+	   (250, "Pretzels", "Snacks", 4.29, 100),
+	   (890, "Zelda: Breath of the Wild 2", "Video Games", 59.99, 20),
+	   (717, "Two and a Half Men Season 6 DVD", "TV Shows", 19.99, 14),
+	   (624, "Super Special Sunglasses", "Clothing", 89.99, 3)
+
 
